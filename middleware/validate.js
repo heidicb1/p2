a0ccc1b3b06d3fe955474d9e9c40d292a926ddfc
heidicb1 <131500888+heidicb1@users.noreply.github.com>
2023-10-02 21:14:14 -0600

    validation

--- middleware/validate.js
+++ middleware/validate.js
@@ -9,6 +9,12 @@ const saveProfessor = (req, res, next) => {
   // Define validation rules for the 'firstName' field
   const validationRules = [
     body('firstName').notEmpty().withMessage('First name is required'),
+    body('lastName').notEmpty().withMessage('Last name is required'),
+    body('hireYear').notEmpty().withMessage('Hire Year is required'),
+    body('department').notEmpty().withMessage('Department is required'),
+    body('title').notEmpty().withMessage('Title is required'),
+    body('email').notEmpty().withMessage('Email is required'),
+    body('course').notEmpty().withMessage('Course is required')
     // Add more validation rules for other fields if needed
   ];
 
@@ -35,6 +41,14 @@ const saveStudent = (req, res, next) => {
   // Define validation rules for the 'firstName' field
   const validationRules = [
     body('firstName').notEmpty().withMessage('First name is required'),
+    body('lastName').notEmpty().withMessage('Last name is required'),
+    body('age').notEmpty().withMessage('Age is required'),
+    body('gender').notEmpty().withMessage('Gender is required'),
+    body('major').notEmpty().withMessage('Major is required'),
+    body('gpa').notEmpty().withMessage('GPA is required'),
+    body('enrollmentDate').notEmpty().withMessage('Enrollment date is required'),
+    body('projectedGraduationDate').notEmpty().withMessage('Projected graduation date is required'),
+    body('email').notEmpty().withMessage('Email is required')
     // Add more validation rules for other fields if needed
   ];
 
