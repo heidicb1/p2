e49e16f9ff0b3bfc6d6ef05e7ee91b7ce42468f9
heidicb1 <131500888+heidicb1@users.noreply.github.com>
2023-10-02 20:28:10 -0600

    index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 
 router.use('/', require('./swagger'));
-router.get('/', (req, res) => {res.send('Welcome');});
 router.use('/professor', require('./professor'));
 router.use('/student', require('./student'));
 
