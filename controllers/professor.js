ec3f9563870feebe20e8a254bc1d0eb616bf63db
heidicb1 <131500888+heidicb1@users.noreply.github.com>
2023-10-02 18:37:34 -0600

    professor.js

--- controllers/professor.js
+++ controllers/professor.js
@@ -6,37 +6,31 @@ const ObjectId = require('mongodb').ObjectId;
 
 
 const getAll = async (req, res) => {
-    // #swagger.tags=['Users] tags keep things together
-    // variable that connects to the database
-    // Use mongodb connection 
-    // .db() is a mongodb function to get the database with opt. parameter 
-    // the opt. parameter allows one to pass in the database name GOOD FOR multi database
-    // ex. mongodb.initDb.getDatabase().db('users').collection('users').find()
-    // However with this project just add users to the of the .env file GOOD FOR single database
-    // Find is a find command searches for wild cards or like and differnt fields
-    const result = await mongodb.getDatabase().db().collection('users').find();
-    // Take results convert to an array then take teh object that comes back and call it users
+    // #swagger.tags=['professor] tags keep things together
+
+    const result = await mongodb.getDatabase().db().collection('professor').find();
+    // Take results convert to an array then take teh object that comes back and call it professor
     // then it passes into anonymous function
-    result.toArray().then((users) => {
+    result.toArray().then((professor) => {
         // Attach this to response object
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users)
+        res.status(200).json(professor)
     }); // Can add a .catch err
 };
 const getSingle = async (req, res) => {
-    // #swagger.tags=['Users]
+    // #swagger.tags=['professor]
     // Just want the first user in the array
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('users').find( {_id: userId} );
-    result.toArray().then((users) => {
+    const result = await mongodb.getDatabase().db().collection('professor').find( {_id: userId} );
+    result.toArray().then((professor) => {
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users)
+        res.status(200).json(professor)
     }); // Can add a .catch err
 };
 
 // In order for this to work we need a body parser in the app.js
 const createUser = async (req, res) => {
-    // #swagger.tags=['Users]
+    // #swagger.tags=['professor]
     const user = {
         
         firstName: req.body.firstName,
@@ -48,7 +42,7 @@ const createUser = async (req, res) => {
         course: req.body.course
     };
     // Create user and user id
-    const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
+    const response = await mongodb.getDatabase().db().collection('professor').insertOne(user);
     // Check the response - basically if there is something there then it work 
     if (response.acknowledged) {
         res.status(204).send();
@@ -60,7 +54,7 @@ const createUser = async (req, res) => {
 };
 
 const updateUser = async (req, res) => {
-    // #swagger.tags=['Users]
+    // #swagger.tags=['professor]
     const userId = new ObjectId(req.params.id);
     const user = {
         firstName: req.body.firstName,
@@ -72,7 +66,7 @@ const updateUser = async (req, res) => {
         course: req.body.course
     };
     // Get user id and update it
-    const response = await mongodb.getDatabase().db().collection('users').replaceOne({ _id: userId }, user);
+    const response = await mongodb.getDatabase().db().collection('professor').replaceOne({ _id: userId }, user);
     // Check the response - basically if there is something there then it work 
     if (response.modifiedCount > 0) {
         res.status(204).send();
@@ -84,10 +78,10 @@ const updateUser = async (req, res) => {
 };
 
 const deleteUser = async (req, res) => {
-    // #swagger.tags=['Users]
+    // #swagger.tags=['professor]
     const userId = new ObjectId(req.params.id);
     // Get user information and delete it
-    const response = await mongodb.getDatabase().db().collection('users').deleteOne({ _id: userId }, true);
+    const response = await mongodb.getDatabase().db().collection('professor').deleteOne({ _id: userId }, true);
     // Check the response - basically if there is something there then it work 
     if (response.deletedCount > 0) {
         res.status(204).send();
